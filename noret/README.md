7771d06c9fa8b0b16669f03887b34696d541ea44
Longo Development <87103946+LongoDevelops@users.noreply.github.com>
2022-07-20 22:59:50 -0400

    Add files via upload

---
+++ noret/README.md
@@ -0,0 +1,6 @@
+Longo Development 
+
+
+I made this so that you can have a reticle for vMenu/esx servers
+
+Join my discord - https://discord.gg/w6aPhqn4t4
---
+++ noret/client.lua
@@ -0,0 +1,28 @@
+Citizen.CreateThread(function()
+local Sniper = false 
+local SniperHash = 100416529
+local heavySniper = 205991906
+local MarksMan = -952879014
+    while true do
+        local player = GetPlayerPed(-1)
+        Citizen.Wait(0)
+
+        local weaponHash = GetSelectedPedWeapon(player)
+
+        if weaponHash == SniperHash then
+            Sniper = true
+        else 
+            if weaponHash == heavySniper then
+                Sniper = true
+            else 
+                if weaponHash == MarksMan then
+                    Sniper = true
+                end
+                
+                if not Sniper then
+                    HideHudComponentThisFrame(14)
+                end
+            end
+        end
+    end
+end)
